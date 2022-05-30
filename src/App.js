163e51ed76ded194236e83e638ee6f8d947266b0
ABHISHEK-SJ <abhisheksjayanna@gmail.com>
2022-05-30 23:34:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cards from './components/Cards';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        GLIMPSE- A Game using React By- Abhishek S J
+      </h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
